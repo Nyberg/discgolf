0e1951f2fd67600dc8685ac60fdae8ab68e56f7a
Johannes Nyberg <johannes.nyb@gmail.com>
2015-02-20 17:52:51 +0100

    efna

--- app/controllers/HoleController.php
+++ app/controllers/HoleController.php
@@ -538,9 +538,6 @@ class HoleController extends \BaseController {
 
         $round = Round::find($id);
 
-
-        if($model == 'round') {
-
             $tee = Tee::where('id', $round->tee_id)->firstOrFail();
             $tees = Tee::with('round')->where('id', $round->tee_id)->get();
             $round = Round::where('id', $round->id)->firstOrFail();
@@ -549,7 +546,7 @@ class HoleController extends \BaseController {
             $user = $this->stat->generateUserAvg($tee, $user_rounds);
             $stats = $this->stat->generateRound($round, $tee);
             $avg = $this->stat->generateAvg($tees);
-        }
+
 
             $message = [
                 'msg' => 'success',
--- public/admin_js/stats/stats.js
+++ public/admin_js/stats/stats.js
@@ -824,6 +824,8 @@ function getUserRoundsReload(){
         $('.fa-spinner').remove();
         $.get('http://178.62.90.148/getRoundAvgScore', {id: $("#id").val(), model: $("#model").val()}, function (data) {
 
+                console.log(data);
+
                 $('#chart-two').highcharts({
                     chart: {
                         type: 'line'
