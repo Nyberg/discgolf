f7a603a4b848336eb37c517992831ef0a8b6c4b3
Johannes Nyberg <johannes.nyb@gmail.com>
2015-02-24 11:31:20 +0100

    hotfic

--- app/dg/Eventing/EventingsServiceProvider.php
+++ app/dg/Eventing/EventingsServiceProvider.php
@@ -7,10 +7,10 @@ class EventingsServiceProvider extends ServiceProvider {
 
     public function register()
     {
-        $listeners = $this->app['config']->get('Dg.listeners');
+        $listeners = $this->app['config']->get('discgolf.listeners');
         foreach($listeners as $listener)
         {
-            $this->app['events']->listen('Dg.*', $listener);
+            $this->app['events']->listen('discgolf.*', $listener);
         }
     }
 }
