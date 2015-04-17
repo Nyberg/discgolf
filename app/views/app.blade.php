a5f04aebbc71e4b5a224c6d690d8d8aff202bb16
Nyberg <johannes.nyb@gmail.com>
2015-04-17 14:00:48 +0200

    fix

--- app/views/app.blade.php
+++ app/views/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/> <!--320-->
     <meta name="description" content="">
     <meta name="author" content="Johannes Nyberg">
