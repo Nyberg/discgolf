26ffc7de5ba15e2992c88e2d32530bfc754b8fa3
Nyberg <johannes.nyb@gmail.com>
2015-04-17 13:32:07 +0200

    app

--- app/views/app.blade.php
+++ app/views/app.blade.php
@@ -2,170 +2,100 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/> <!--320-->
     <meta name="description" content="">
-    <meta name="author" content="Dashboard">
-    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+    <meta name="author" content="Johannes Nyberg">
+    <meta name="keyword" content="">
+    <link rel="shortcut icon" type="image/png" href="/img/penguin-png.png"/>
+    <title>Penguin Project | Alfa</title>
 
-{{HTML::style('admin_css/css/bootstrap.css')}}
-{{HTML::style('admin_css/font-awesome/css/font-awesome.css')}}
-{{HTML::style('admin_css/css/zabuto_calendar.css')}}
-{{HTML::style('admin_js/gritter/css/jquery.gritter.css')}}
-{{HTML::style('admin_css/lineicons/style.css')}}
-{{HTML::style('admin_css/css/style.css')}}
-{{HTML::style('admin_css/css/style-responsive.css')}}
-{{HTML::style('admin_css/css/datepicker.css')}}
-{{HTML::style('css/lightbox.css')}}
-{{HTML::script('js/jquery-1.11.0.min.js')}}
-{{HTML::script('js/lightbox.min.js')}}
-{{HTML::script(Charts.js')}}
-{{HTML::script('packages/jleach/laravelmce/js/tinymce/tinymce.min.js')}}
-{{HTML::script('https://maps.googleapis.com/maps/api/js?key=AIzaSyAqPB9cgeF7VZw7JTd2qYD2K4TPbCNDicc&sensor=false')}}
-{{HTML::script('cordova.js')}}
-
+    {{HTML::style('admin_css/css/bootstrap2.css')}}
+    {{HTML::style('admin_css/font-awesome/css/font-awesome.css')}}
+    {{HTML::style('admin_css/css/style.css')}}
+    {{HTML::style('admin_css/css/style-responsive.css')}}
+    {{HTML::style('admin_css/css/datepicker.css')}}
+    {{HTML::style('admin_css/css/jquery-ui.css')}}
+    {{HTML::script('http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js')}}
+    {{HTML::script('http://code.jquery.com/jquery-1.11.0.min.js')}}
+    {{HTML::script('admin_js/form-validator/jquery.form-validator.js')}}
+    {{HTML::script('admin_js/jquery-ui.min.js')}}
+    {{HTML::script('packages/jleach/laravelmce/js/tinymce/tinymce.min.js')}}
+    {{HTML::script('//tinymce.cachefly.net/4.1/tinymce.min.js')}}
 
 
+<script>tinymce.init({selector:'textarea'});</script>
+
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.1.47/jquery.form-validator.min.js"></script>
+
+  <body data-spy="scroll">
+
+    <section id="container">
+   <div class="row-fluid back">
+    <div class="col-lg-10 col-md-offset-1">
+
+    </div>
+    </div>
+
+    <section id="main-content">
+      <section class="wrapper site-min-height">
+
+        <div class="row">
+            <div class="showback">
+
+                @include('layouts/include/flash') <!-- Visar alla flashmeddelanden som skickas ut av systemet -->
+
+                @yield('content')
+
+            </div>
+        </div>
+
+      </section><!--/wrapper -->
+    </section><!-- /MAIN CONTENT -->
+
+    <footer>
+        <div class="navbar navbar-inverse navbar-fixed-bottom">
+            <div class="container">
+              	<div class="navbar-header text-center margin-top">
+                      <div class="col-xs-4"><a href="/"><i class="fa fa-home fa-3x green"></i></a></div>
+                      <div class="col-xs-4"><a href=""><i class="fa fa-home fa-3x green"></i></a></div>
+                      <div class="col-xs-4"><a href=""><i class="fa fa-home fa-3x green"></i></a></div>
+                </div>
+
+            </div>
+        </div>
+    </footer>
 
 
-<script type="text/javascript">
-    // Wait for device API libraries to load
-    //
-    function onLoad() {
-        document.addEventListener("deviceready", onDeviceReady, false);
-    }
 
-    // device APIs are available
-    //
-    function onDeviceReady() {
-        document.addEventListener("resume", onResume, false);
-    }
 
-    // Handle the resume event
-    //
-    function onResume() {
-    }
-</script>
+<!-- js placed at the end of the document so the pages load faster -->
+{{HTML::script('admin_js/bootstrap.min.js')}}
+{{HTML::script('admin_js/jquery.dcjqaccordion.2.7.js', ['class'=>'include'])}}
+{{HTML::script('admin_js/jquery.scrollTo.min.js')}}
+{{HTML::script('admin_js/jquery.nicescroll.js')}}
+<!--common script for all pages-->
+{{HTML::script('admin_js/common-scripts.js')}}
+{{HTML::script('admin_js/bootstrap-datepicker.js')}}
+<!--script for this page-->
+{{HTML::script('admin_js/app.js')}}
+
+<script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="http://cdn.oesmith.co.uk/morris-0.4.3.min.js"></script>
+
+@yield('scripts')
 
 <script>
-tinymce.init({
-    selector: "textarea"
-});
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-60693522-1', 'auto');
+  ga('send', 'pageview');
 </script>
 
-
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<style>body{padding-top: 85px;}</style>
-<!--
-    <link href="assets/css/docs.css" rel="stylesheet">
-    <link href="assets/js/google-code-prettify/prettify.css" rel="stylesheet">
-     <link href="assets/css/style.css" rel="stylesheet"> -->
-
-  <body data-spy="scroll" data-target=".bs-docs-sidebar" onload="onLoad()">
-
-    <section id="container" >
-
-    @include('layouts/include/menu')
-
-          <section id="main-content">
-              <section class="wrapper site-min-height">
-
-              	<div class="row mt">
-              		<div class="col-lg-12">
-              		<div class="showback">
-
-              		 @include('layouts/include/flash')
-
-                    @yield('content')
-
-                  		</div>
-                  	</div>
-                  	</div>
-
-        		</section><! --/wrapper -->
-              </section><!-- /MAIN CONTENT -->
-
-    <!-- js placed at the end of the document so the pages load faster -->
-    {{HTML::script('admin_js/jquery.js')}}
-    {{HTML::script('admin_js/jquery-1.8.3.min.js')}}
-    {{HTML::script('admin_js/bootstrap.min.js')}}
-    {{HTML::script('admin_js/jquery.dcjqaccordion.2.7.js', ['class'=>'include'])}}
-    {{HTML::script('admin_js/jquery.scrollTo.min.js')}}
-    {{HTML::script('admin_js/jquery.nicescroll.js')}}
-    {{HTML::script('admin_js/jquery.sparkline.js')}}
-
-
-    <!--common script for all pages-->
-    {{HTML::script('admin_js/common-scripts.js')}}
-
-    {{HTML::script('admin_js/gritter/js/jquery.gritter.js')}}
-    {{HTML::script('admin_js/gritter-conf.js')}}
-    {{HTML::script('admin_js/bootstrap-datepicker.js')}}
-
-    <!--script for this page-->
-    {{HTML::script('admin_js/sparkline-chart.js')}}
-    {{HTML::script('admin_js/zabuto_calendar.js')}}
-
-@section('scripts')
-
-<script type="text/javascript">
-        $(document).ready(function () {
-        var unique_id = $.gritter.add({
-            // (string | mandatory) the heading of the notification
-            title: 'Welcome to Dashgum!',
-            // (string | mandatory) the text inside the notification
-            text: 'Hover me to enable the Close Button. You can hide the left sidebar clicking on the button next to the logo. Free version for <a href="http://blacktie.co" target="_blank" style="color:#ffd777">BlackTie.co</a>.',
-            // (string | optional) the image to display on the left
-            image: 'assets/img/ui-sam.jpg',
-            // (bool | optional) if you want it to fade out on its own or just sit there
-            sticky: true,
-            // (int | optional) the time you want it to be alive for before fading out
-            time: '',
-            // (string | optional) the class name you want to apply to that specific message
-            class_name: 'my-sticky-class'
-        });
-
-        return false;
-        });
-	</script>
-
-	<script type="application/javascript">
-        $(document).ready(function () {
-            $("#date-popover").popover({html: true, trigger: "manual"});
-            $("#date-popover").hide();
-            $("#date-popover").click(function (e) {
-                $(this).hide();
-            });
-
-            $("#my-calendar").zabuto_calendar({
-                action: function () {
-                    return myDateFunction(this.id, false);
-                },
-                action_nav: function () {
-                    return myNavFunction(this.id);
-                },
-                ajax: {
-                    url: "show_data.php?action=1",
-                    modal: true
-                },
-                legend: [
-                    {type: "text", label: "Special event", badge: "00"},
-                    {type: "block", label: "Regular event", }
-                ]
-            });
-        });
-
-
-        function myNavFunction(id) {
-            $("#date-popover").hide();
-            var nav = $("#" + id).data("navigation");
-            var to = $("#" + id).data("to");
-            console.log('nav ' + nav + ' to: ' + to.month + '/' + to.year);
-        }
-
-        </script>
-
-@stop
 
   </body>
 </html>
--- app/views/app/start.blade.php
+++ app/views/app/start.blade.php
@@ -1,4 +1,4 @@
-@extends('master')
+@extends('app')
 
 @section('content')
 
